30299f5e38a80a0fd7cfa15e23a1691c6c498174
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-05-15 06:41:26 +0700

    Added link in home to board pages, and in board pages to home

--- boards/tests.py
+++ boards/tests.py
@@ -6,15 +6,31 @@ from boards.models import Board
 
 class HomeTests(TestCase):
     def setUp(self):
+        self.board = Board.objects.create(name="Django", description="Django board.")
         self.url = reverse("boards:home")
+        self.res = self.client.get(self.url)
 
     def test_home_success_status_code(self):
-        res = self.client.get(self.url)
-        self.assertEqual(res.status_code, 200)
+        """
+            Test home status code
+        :return:
+        """
+        self.assertEqual(self.res.status_code, 200)
 
     def test_home_view(self):
+        """
+            test home view
+        :return:
+        """
         view = resolve(self.url)
         self.assertEqual(view.func, home)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        """
+            test home page contains link to tpics page
+        """
+        board_topics_url = reverse("boards:board_topics", kwargs={"id": self.board.id})
+        self.assertContains(self.res, f'href="{board_topics_url}"')
 
 
 class BoardTopicsTests(TestCase):
@@ -44,3 +60,12 @@ class BoardTopicsTests(TestCase):
         """
         view = resolve(self.get_url(1))
         self.assertEqual(view.func, board_topics)
+
+    def test_board_topics_page_have_link_to_home_page(self):
+        """
+            test board page have link back to home page
+        :return:
+        """
+        res = self.client.get(self.get_url(1))
+        home_url = reverse("boards:home")
+        self.assertContains(res, f'href="{home_url}"')
