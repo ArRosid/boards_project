7a35b2aaaadb63f3025d9ae7670c202b525aae6a
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-05-16 06:12:36 +0700

    Added logout features

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from accounts.views import signup
 
 
 app_name = "accounts"
 
-urlpatterns = [path("signup/", signup, name="signup")]
+urlpatterns = [
+    path("signup/", signup, name="signup"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+]
